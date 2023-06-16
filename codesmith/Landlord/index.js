812bdecd956e3d50844e484ee655faf1dd527147
Denton Wong <denton.wong@gmail.com>
2023-06-16 12:24:09 -0700

    pivoting to using d3.js instead of node

--- codesmith/Landlord/index.js
+++ codesmith/Landlord/index.js
@@ -1,15 +1,34 @@
-const csv = require('csv-parser')
-const fs = require('fs')
-const results = [];
+// const csv = require('csv-parser')
+// const fs = require('fs')
+// const results = [];
 
-fs.createReadStream('transactions.csv')
-  .pipe(csv({mapHeaders: ({ header, index }) => header.toLowerCase()}))
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
+// //alert("Test 1");
+
+// fs.createReadStream('transactions.csv')
+//   .pipe(csv({mapHeaders: ({ header, index }) => header.trim()}))
+//   .on('data', (data) => results.push(data))
+//   .on('end', () => {
 
 
-    console.log(results);
+//     console.log(results);
+//     alert("Test 2");
+//   });
+// //document.write("test");
+// //alert("Test 2");
 
-  });
-document.write("test");
-alert("This alert box was called with the onload event");
+const myForm = document.getElementById("myForm");
+const csvFile = document.getElementById("csvFile");
+
+myForm.addEventListener("submit", function (e) {
+e.preventDefault();
+const input = csvFile.files[0];
+const reader = new FileReader();
+
+reader.onload = function (e) {
+    const text = e.target.result;
+    const data = d3.csvParse(text);
+    document.write(JSON.stringify(data));
+};
+
+reader.readAsText(input);
+});
